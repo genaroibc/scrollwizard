1d45d24a4f249e55f527a97836b11402257f4ed8
genaroibc <bonavitaismael@gmail.com>
2023-07-21 21:39:02 -0300

    [FEAT]: improve seo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,11 +7,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Scrollbar Wizard - Customize your scrollbar with ease',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description'
+      content: 'Create your own scrollbar'
     }
   ]
 };
